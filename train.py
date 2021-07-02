5136c51c525c643aec2c19cad1197ad1f6667e9a
Alexandros Metsai <alexmetsai@gmail.com>
2021-07-02 10:25:16 +0300

    load and split data

--- train.py
+++ train.py
@@ -19,3 +19,15 @@ model = keras.Model(inputs, outputs=outputs)
 # Define optimizer and loss function.
 optimizer = keras.optimizers.SGD(learning_rate=1e-3)
 loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+# Load training dataset.
+batch_size = 64
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+x_train = np.reshape(x_train, (-1, 784))
+x_test = np.reshape(x_test, (-1, 784))
+
+# Create validation set from last 10,000 samples.
+x_val = x_train[-10000:]
+y_val = y_train[-10000:]
+x_train = x_train[:-10000]
+y_train = y_train[:-10000]
