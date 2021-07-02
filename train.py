3609acb58eb1ac3f06e3dbabb3b38f743ad2b031
Alexandros Metsai <alexmetsai@gmail.com>
2021-07-02 10:24:34 +0300

    optimizer and loss function

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+# Alexandros I. Metsai
+# alexmetsai@.gmail.com
+
 import tensorflow as tf
 import numpy as np
 import time
@@ -12,3 +15,7 @@ x1 = layers.Dense(64, activation='relu')(inputs)
 x2 = layers.Dense(64, activation='relu')(x1)
 outputs = layers.Dense(10, name='predictions')(x2)
 model = keras.Model(inputs, outputs=outputs)
+
+# Define optimizer and loss function.
+optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
